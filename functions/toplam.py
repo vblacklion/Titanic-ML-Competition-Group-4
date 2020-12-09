ee12160df178c08631cc2a480a8dbfb4b20f3e20
vblacklion <buonovulcano@gmail.com>
2020-12-09 19:56:53 +0100

    deneme modified and function folder added

--- functions/toplam.py
+++ functions/toplam.py
@@ -1,5 +1,14 @@
 def toplam(x, y):
     """
     verilen iki sayıyı toplar.
+
+    >>>> toplam(2,3)
+    >>>> 5
     """
     return x + y
+
+def ortalama(x,y):
+    """
+    verilen iki sayının ortalamasını alır.
+    """
+    return (x+y)/2.0

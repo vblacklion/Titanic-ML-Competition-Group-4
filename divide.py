977d7bdb70adfa0991903fd671abc2da95e98ec9
vblacklion <buonovulcano@gmail.com>
2021-01-06 22:31:05 +0100

    son kontroller yapıldı
    gereksiz dosyalar silindi

--- divide.py
+++
@@ -1,6 +0,0 @@
-def divide(a, b):
-    try:
-        result = a/float(b)
-        return result
-    except ZeroDivisionError:
-        print("'0' a bölünme hatası")
--- eksilt2.py
+++
@@ -1,2 +0,0 @@
-def eksilt2(a,b):
-    return a-b
